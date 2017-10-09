cf308686e6c92584022f86081daafeb9ec97c248
dedady157 <bailey.drahoss@outlook.com>
2017-10-09 20:45:20 +0200

    Upload

---
+++ RemoteRAMv1/FindPrimeWithRR/FindPrimeWithRR.ino
@@ -0,0 +1,131 @@
+//Discovery /w Remote RAM
+PROGMEM const byte END = 255;
+PROGMEM const byte REQInt = 166;
+PROGMEM const byte SETInt = 167;
+PROGMEM const byte SayByte = 10;
+PROGMEM const byte CreateAddress = 11;
+PROGMEM const byte HELO[6] {12, 12, 44, 51, 52, 112};
+PROGMEM const int SizeOfHELO = 6;
+PROGMEM const int ReadTimeoutTime = 200;
+
+int LargestAddress = 0;
+
+bool ReadErrOut = false;
+void Read(byte Buffer[], int Count)
+{
+  int start = millis();
+  int pos = 0;
+  while (millis() - start < ReadTimeoutTime && pos != Count - 1)
+  {
+    if (Serial.available() > 0) {
+      Buffer[pos] = Serial.read();
+      pos++;
+      start = millis();
+    }
+  }
+  if (millis() - start > ReadTimeoutTime)
+    ReadErrOut = true;
+    else
+    ReadErrOut = false;
+}
+
+void Connect()
+{
+  byte HL[SizeOfHELO];
+  Read(HL, SizeOfHELO);
+  bool Write = ReadErrOut;
+  if (Write)
+  {
+    for (int x = 0; x < SizeOfHELO; x++)
+    {
+      if (HELO[x] != HL[x])
+      {
+        Write = false;
+        Connect();
+        break;
+      }
+    }
+    if (Write)
+    {
+      Serial.write((const char*)HELO);
+    }
+  }
+  else
+  {
+    Connect();
+  }
+}
+
+void CreateInt(int value)
+{
+  Serial.write(CreateAddress);//create new Address
+  SetInt(LargestAddress, value);
+  LargestAddress++;//step up largest address
+}
+void SetInt(int addr, int value)
+{
+  Serial.write(SETInt);//set that address
+  Serial.write(highByte(addr));//send address
+  Serial.write(lowByte(addr));
+  Serial.write(highByte(value));//send new value
+  Serial.write(lowByte(value));
+}
+
+union  ByteArrayToint { //converter
+  byte array[2];
+  int16_t integer;
+};
+
+int GetInt(int addr)
+{
+  Serial.write(REQInt);//set that address
+  Serial.write(highByte(addr));//send address
+  Serial.write(lowByte(addr));
+  byte value[2];
+  Read(value, 2);
+  ByteArrayToint Conv = {value[0], value[1]};
+  return Conv.integer;
+}
+
+void say(String what)
+{
+  Serial.write(SayByte);
+  Serial.print(what + "\n"); //STD Encoding is ASCII
+}
+
+//-------------------------------------------------------------------------------------------------------------
+int Curr = 1;
+
+void setup() {
+  Serial.begin(9600);
+  //Get connection
+  Connect();
+}
+
+void loop() {//do discovery mode;
+  if (Curr > 10) {
+    if (((String)Curr).endsWith("1") || ((String)Curr).endsWith("7") || ((String)Curr).endsWith("9"))
+      Curr += 2; //to 3, 9
+    else if (((String)Curr).endsWith("3"))
+      Curr += 4; //to 7
+    else
+      Curr++;
+  }
+  bool IsPrime = true;
+  for (int x = 0; x < LargestAddress; x++)
+  {
+    int tmp = GetInt(x);
+    if (tmp % Curr == 0)
+    {
+      IsPrime = false;
+      break;
+    }
+  }
+  if (IsPrime)
+  {
+    CreateInt(Curr);
+    String what = "Found ";
+    what += String(Curr);
+    say(what);
+  }
+}
---
+++ RemoteRAMv1/RemoteRAMv1/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+using System.Text;
+namespace RemoteRAMv_1
+{
+    class Program
+    {
+        const int ConnectionTimeOutRetryTime = 2000;
+        const int ReadTimeout = 2000;
+        const int BitRate = 115200;
+        const string RRPort = "COM3";
+        static readonly Encoding DEV_Encoding = Encoding.ASCII;//Arduino
+        static readonly byte[] HELO = new byte[] { 0x48,0x45,0x4c,0x4f };
+        static SerialPort DEV;
+        static byte[] MEM;
+        static bool DEBUG;
+
+        static void Main(string[] args)
+        {
+
+            if (args.Length > 0 && args[0] == "-v") DEBUG = true; else DEBUG = false;
+            DEBUG = true;
+
+            Console.Title = "RemoteRAM v1 Beta PreRelease";
+            Console.WriteLine("Connecting to Port (" + RRPort + ")");
+            DEV = new SerialPort(RRPort, BitRate);//open port
+            DEV.Encoding = DEV_Encoding;
+            DEV.Open();
+            Connect();
+            WORK();
+            Console.Write("Press any Enter to close console:");
+            Console.ReadKey();
+        }
+
+        //main loop
+        static void WORK()
+        {
+            bool Running = true;
+            while(DEV.IsOpen && Running)
+            {
+                if (DEV.BytesToRead > 0)
+                {
+                    byte Flag = ReadByte();
+                    switch(Flag)
+                    {
+                        case (255)://END
+                            Console.WriteLine("Closing by DEV");
+                            DEV.Close();
+                            Running = false;
+                            //Write(0);
+                            break;
+                        case (166)://readInt
+                            short addr=BitConverter.ToInt16(Read(2), 0);
+                            ReadInt(addr);
+                            break;
+                        case (167)://setInt
+                            short Addr, value;
+                            byte[] input = Read(4);
+                            Addr = BitConverter.ToInt16(input, 0);
+                            value = BitConverter.ToInt16(input, 2);
+                            SetInt(Addr, value);
+                            //Write(0);
+                            break;
+                        case (11)://Alloc
+                            AllocNewInt();
+                            //Write(0);
+                            break;
+
+                        case (10)://say
+                            Console.WriteLine("DEV Said:"+DEV.ReadLine());
+                            //Write(0);
+                            break;
+
+                        default:
+                            Console.WriteLine("unknown Flag " + Flag);
+                            break;
+                    }
+                }
+            }
+            if (Running)
+                Console.WriteLine("DEV Disconnect, Closing");
+        }
+
+        static void AllocNewInt()
+        {
+            if (DEBUG)
+                Console.WriteLine("Alloc Creating New Address");
+            Array.Resize<byte>(ref MEM, MEM.Length + 2);
+        }
+        static void SetInt(short Address,short NewValue)
+        {
+            if (DEBUG)
+                Console.WriteLine("Setting " + Address + " to " + NewValue);
+            Array.Copy(BitConverter.GetBytes(NewValue), 0, MEM, Address * 2, 2);
+            //Set Value
+        }
+        static void ReadInt(short Address)
+        {
+            if (DEBUG)
+                Console.WriteLine($"Echoing *{Address} = '{BitConverter.ToInt16(MEM, Address * 2)}'");
+            byte[] buf = new byte[] { MEM[Address * 2], MEM[(Address * 2) + 1] };
+            Write(buf);
+        }
+
+        static byte[] Read(int count)
+        {
+            if(DEBUG)
+                Console.WriteLine("Reading " + count + " bytes");
+            byte[] buffer = new byte[count];
+            DateTime TMR = DateTime.Now;
+            while (DEV.BytesToRead < count && DateTime.Now - TMR < TimeSpan.FromMilliseconds(ReadTimeout));
+            if (DEV.BytesToRead < count)
+            {
+                Console.WriteLine("Failed to read");
+                Console.WriteLine("BUFFER=" + DEV_Encoding.GetString(buffer));
+                return new byte[count];//return 0n
+            }
+            else
+            {
+                DEV.Read(buffer, 0, count);
+                return buffer;
+            }
+        }
+        static byte ReadByte()
+        {
+            return Read(1)[0];
+        }
+
+        static void Write(byte[] x)
+        {
+            if (DEBUG)
+                Console.WriteLine("Writing "+x.Length+" bytes");
+            DEV.Write(x, 0, x.Length);
+        }
+        static void Write(byte x)
+        {
+            if (DEBUG)
+                Console.WriteLine("Writing 1 byte");
+            byte[] buf = new byte[] { x };
+            DEV.Write(buf, 0, 1);
+        }
+
+        static void Connect()
+        {
+            try
+            {
+                Write(HELO);//say hi
+                MEM = new byte[0];//de nullify and reset
+                byte[] HL = Read(HELO.Length);
+
+                for (int x = 0; x < HL.Length; x++)
+                {
+                    if (HELO[x] != HL[x])
+                    {
+                        Console.WriteLine("HELO Failed");
+                        Console.WriteLine("Retrying in " + ConnectionTimeOutRetryTime);
+                        Thread.Sleep(ConnectionTimeOutRetryTime);
+                        Connect();
+                        break;
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception. Retrying in "+ ConnectionTimeOutRetryTime);
+                Thread.Sleep(ConnectionTimeOutRetryTime);
+                Connect();
+            }
+        }
+    }
+}
